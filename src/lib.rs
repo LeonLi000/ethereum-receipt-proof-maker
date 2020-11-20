3f8d022bc808c864b15f88f3e92f37d619837d67
LeonLi000 <matrix.skygirl@gmail.com>
2020-11-20 15:59:06 +0800

    minor change

--- src/lib.rs
+++ src/lib.rs
@@ -60,41 +60,22 @@ pub fn generate_eth_proof(
     tx_hash: String,
     endpoint: String,
 ) -> Result<EthSpvProof, errors::AppError> {
-    let proof = State::init(
-        convert_hex_to_h256(tx_hash.clone())?,
-        tx_hash.clone(),
-        Some(endpoint.clone()),
-    )
-    .and_then(get_block_from_tx_hash_in_state_and_set_in_state)
-    .and_then(get_all_receipts_from_block_in_state_and_set_in_state)
-    .and_then(get_tx_index_and_add_to_state)
-    .and_then(get_receipts_trie_and_set_in_state)
-    .and_then(get_branch_from_trie_and_put_in_state)
-    .and_then(get_hex_proof_from_branch_in_state);
     let mut res_receipt =
         get_receipt_from_tx_hash(endpoint.clone().as_str(), tx_hash.clone().as_str());
     let mut stream = RlpStream::new();
     let receipt =  res_receipt.unwrap();
-    println!("{:?}", receipt);
-
     let logs = &receipt.logs;
     receipt.rlp_append(&mut stream);
-    let receipt_data = hex::encode(stream.out());
-    // let mut log_data = String::new();
     let mut log_index = -1;
     let mut is_exist = false;
 
-    // let mut eth_spv_proof = EthSpvProof::default();
     let mut eth_spv_proof = EthSpvProof {
         log_index: -1,
         receipt_index: receipt.transaction_index.as_u64(),
-        receipt_data,
         block_hash: receipt.block_hash,
-        proof: proof.unwrap(),
         ..Default::default()
     };
     for item in logs {
-        println!("{:?}", item);
         log_index += 1;
         if hex::encode(item.clone().topics[0].0) == constants::LOCK_EVENT_STRING {
             let event = Event {
@@ -143,7 +124,6 @@ pub fn generate_eth_proof(
                 data: item.clone().data,
             };
             let result = event.parse_log(raw_log).unwrap();
-            println!("parse event log: {:?}", result);
             for v in result.params {
                 match v.name.as_str() {
                     "token" => {
@@ -167,10 +147,7 @@ pub fn generate_eth_proof(
                     _ => {}
                 }
             }
-            let mut stream = RlpStream::new();
-            item.rlp_append(&mut stream);
             eth_spv_proof.log_index = log_index;
-            eth_spv_proof.log_entry_data = hex::encode(stream.out());
             is_exist = true;
             break;
         }
@@ -186,8 +163,8 @@ pub fn generate_eth_proof(
 
 #[test]
 fn test_get_hex_proof() {
-    let endpoint = "http://127.0.0.1:8545";
-    let tx_hash = "0x26ac5c1ecd1a3d3bd8d8715c5302420dc29c2840dba3ff88de69d4fe85b339fd";
+    let endpoint = "https://mainnet.infura.io/v3/9c7178cede9f4a8a84a151d058bd609c";
+    let tx_hash = "0xb540248a9cca048c5861dec953d7a776bc1944319b9bd27a462469c8a437f4ff";
     let proof = generate_eth_proof(String::from(tx_hash), String::from(endpoint));
     match proof {
         Ok(proof) => {
@@ -197,6 +174,7 @@ fn test_get_hex_proof() {
             println!("{:?}", err);
         }
     }
+    ///////////////////////////////f901b6f901b3822080b901adf901aa0182d0d9b9010000000000000000000000000000000000000000000000000000000000000200000000000000000000000000000000010000000000000000000000000000000000000000000000000000000408000000000000000000000000000000000000000000000000000000000000000000000001000000000000000000000010000000000000000000000000000000000000000000000000000000400000000000100000000000000000000000000080000000000000000000000000000000000000000000100002000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000002000000000000000000000000f8a1f89f94dac17f958d2ee523a2206206994597c13d831ec7f863a0ddf252ad1be2c89b69c2b068fc378daa952ba7f163c4a11628f55a4df523b3efa00000000000000000000000006cc5f688a315f3dc28a7781717a9a798a59fda7ba00000000000000000000000007e7a32d9dc98c485c489be8e732f97b4ffe3a4cda000000000000000000000000000000000000000000000000000000001a13b860083307830
     // assert_eq!(proof.unwrap(), "f901b2f901af822080b901a9f901a60182d0d9b9010000000000000000000000000000000000000000000000000000000000000200000000000000000000000000000000010000000000000000000000000000000000000000000000000000000408000000000000000000000000000000000000000000000000000000000000000000000001000000000000000000000010000000000000000000000000000000000000000000000000000000400000000000100000000000000000000000000080000000000000000000000000000000000000000000100002000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000002000000000000000000000000f89df89b94dac17f958d2ee523a2206206994597c13d831ec7f863a0ddf252ad1be2c89b69c2b068fc378daa952ba7f163c4a11628f55a4df523b3efa00000000000000000000000006cc5f688a315f3dc28a7781717a9a798a59fda7ba00000000000000000000000007e7a32d9dc98c485c489be8e732f97b4ffe3a4cda000000000000000000000000000000000000000000000000000000001a13b8600");
 }
 
